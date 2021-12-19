94bd05431954f1171d3e05a2c250c55ca611ac8e
Shafeeque OM <5572374-shafeequekrews@users.noreply.gitlab.com>
2021-12-19 10:10:13 +0400

    Vuejs Setups and Initial Project setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div>Abu Dhabi Police</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  // components: {
+  // },
+};
+</script>
+
+<style>
+</style>
